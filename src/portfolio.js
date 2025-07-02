8c6d566dcc0a24cdde6969191ce30eb92400c4d4
Akash Mamidi <101381686+akashmamidi@users.noreply.github.com>
2025-07-02 23:00:52 +0530

    Update portfolio.js

--- src/portfolio.js
+++ src/portfolio.js
@@ -45,7 +45,7 @@ const socialMediaLinks = [
   },
   {
     name: "Facebook",
-    link: "https://www.facebook.com/vishal0x11/",
+    link: "https://www.facebook.com/akashsamamidi/",
     fontAwesomeIcon: "fa-facebook-f", // Reference https://fontawesome.com/icons/facebook-f?style=brands
     backgroundColor: "#1877F2", // Reference https://simpleicons.org/?q=facebook
   },
